04e2e2bfc147cc32dc372a685a49f73a27575b10
zhpinkman <zhivarsourati@gmail.com>
2019-06-01 06:01:15 +0430

    initial clocks added

---
+++ clock.sh
@@ -0,0 +1,4 @@
+# while true; do tput clear; date +"%H : %M : %S" | figlet ; sleep 1; done
+# watch -tn 1 date +%T \|figlet -c
+# watch -t -n1 "date +%T|figlet" | lolcat -a -
+watch -t -n1 "date +%n%n%n%n%X|figlet -t -c"
